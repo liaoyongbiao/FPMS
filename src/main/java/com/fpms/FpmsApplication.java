d72f372281f1afebc6fe2bfbcf9a37a4261636b1
liaoyongbiao <1105235030@qq.com>
2019-06-10 10:14:28 +0800

    项目基本结构

---
+++ src/main/java/com/fpms/FpmsApplication.java
@@ -0,0 +1,13 @@
+package com.fpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpmsApplication.class, args);
+    }
+
+}
